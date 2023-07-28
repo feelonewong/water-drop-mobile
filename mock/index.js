9240dcc3e8edc55ae37ea3de284c096c0ee9ec99
feelonewong <feelonewong@gmail.com>
2023-07-28 20:52:58 +0800

    update: 完成7-10实现apollo-server的mock数据

---
+++ mock/index.js
@@ -0,0 +1,63 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { faker } from "@faker-js/faker/locale/zh_CN";
+const typeDefs = `#graphql
+    type UserType {
+        """ID"""
+        id: String!
+    
+        """姓名"""
+        name: String!
+    
+        """详情"""
+        desc: String!
+    
+        """账户信息"""
+        account: String!
+    }
+    
+    type Query {
+        """使用id查询用户"""
+        find(id: String!): UserType!
+    }
+    
+    type Mutation {
+        """新建用户"""
+        create(params: UserInput!): Boolean!
+    
+        """更新用户"""
+        update(id: String!, params: UserInput!): Boolean!
+    
+        """删除用户"""
+        del(id: String!): Boolean!
+    }
+    
+    input UserInput {
+        name: String!
+        desc: String!
+    }
+`;
+
+const resolvers = {
+  UserType: {
+    name: () => faker.name.fullName(),
+  },
+};
+
+const mocks = {
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => faker.address.city(),
+};
+
+const server = new ApolloServer({
+  schema: addMocksToSchema({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    mocks,
+    preserveResolvers: true,
+  }),
+});
+
+startStandaloneServer(server, { listen: { port: 8888 } });
